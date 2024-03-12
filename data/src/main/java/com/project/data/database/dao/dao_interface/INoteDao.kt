9171540b3806805800622d9cd20950dd3b3fc3a0
Fazer777 <m.soup2016@yandex.ru>
2024-03-12 21:21:09 +0300

    Update INoteDao.kt

--- data/src/main/java/com/project/data/database/dao/dao_interface/INoteDao.kt
+++ data/src/main/java/com/project/data/database/dao/dao_interface/INoteDao.kt
@@ -3,8 +3,8 @@ package com.project.data.database.dao.dao_interface
 import com.project.data.models.Note
 
 interface INoteDao {
-    fun addNote(note : Note)
-    fun deleteNote(itemIndex : Int)
-    fun updateNote(note : Note)
-    fun readAllData() : List<Note>
+    suspend fun addNote(note : Note)
+    suspend fun deleteNote(itemIndex : Int)
+    suspend fun updateNote(note : Note)
+    suspend fun readAllData() : List<Note>
 }
