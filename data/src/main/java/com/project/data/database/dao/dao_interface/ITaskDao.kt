5fb38c960e55a3562018f28f72904bc6e4183808
Fazer777 <m.soup2016@yandex.ru>
2024-03-12 21:21:19 +0300

    Update ITaskDao.kt

--- data/src/main/java/com/project/data/database/dao/dao_interface/ITaskDao.kt
+++ data/src/main/java/com/project/data/database/dao/dao_interface/ITaskDao.kt
@@ -1,14 +1,12 @@
 package com.project.data.database.dao.dao_interface
 
 import com.project.data.models.Task
+import java.time.LocalDate
 
 interface ITaskDao {
-    fun addTask(task : Task) : Int
-
-    fun updateTask(id : Int, task: Task)
-
-
-    fun deleteTask(taskId : Int)
-
-    fun getTasks() : List<Task>
+    suspend fun addTask(task : Task) : Int
+   suspend fun updateTask(taskId: Int, task: Task)
+   suspend fun updateTaskChecked(taskId: Int, checked : Boolean, completionDate:LocalDate)
+    suspend fun deleteTask(taskId : Int)
+    suspend fun getTasks() : List<Task>
 }
