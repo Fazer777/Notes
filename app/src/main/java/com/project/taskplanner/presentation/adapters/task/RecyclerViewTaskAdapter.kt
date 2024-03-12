11c6b54426d24a70cb2fc3a006eabb2ca1b42a28
Fazer777 <m.soup2016@yandex.ru>
2024-03-12 20:51:27 +0300

    Update RecyclerViewTaskAdapter.kt

--- app/src/main/java/com/project/taskplanner/presentation/adapters/task/RecyclerViewTaskAdapter.kt
+++ app/src/main/java/com/project/taskplanner/presentation/adapters/task/RecyclerViewTaskAdapter.kt
@@ -1,26 +1,26 @@
 package com.project.taskplanner.presentation.adapters.task
 
-import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.graphics.Paint;
 import android.graphics.drawable.Icon
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.project.domain.models.TaskInterim
 import com.project.taskplanner.R
 import com.project.taskplanner.databinding.ItemTaskRecyclerviewBinding
+import com.project.taskplanner.presentation.fragments.tasks.LayoutEnum
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
-class RecyclerViewTaskAdapter(val context : Context) : RecyclerView.Adapter<RecyclerViewTaskAdapter.MyViewHolder>() {
+class RecyclerViewTaskAdapter() : RecyclerView.Adapter<RecyclerViewTaskAdapter.MyViewHolder>() {
 
-    public var taskList = ArrayList<TaskInterim>()
+    private var taskList = ArrayList<TaskInterim>()
     private var listener : OnItemClickListener? = null
+    private var layoutEnum : LayoutEnum = LayoutEnum.LINEAR
     inner class MyViewHolder(itemView : View) : ViewHolder(itemView){
 
         private val binding = ItemTaskRecyclerviewBinding.bind(itemView)
@@ -36,24 +36,33 @@ class RecyclerViewTaskAdapter(val context : Context) : RecyclerView.Adapter<Recy
                 taskInterim.completionDate)
 
             itemView.setOnClickListener {
-                listener?.let {
+                listener?.let {listener->
                     val position = adapterPosition
                     if (position != RecyclerView.NO_POSITION){
-                        listener!!.onItemClick(itemView, position)
+                        listener.onItemClick(itemView, position)
                     }
                 }
             }
 
             itemView.setOnLongClickListener {
 
-                listener?.let {
+                listener?.let {listener->
                     val position = adapterPosition
                     if (position != RecyclerView.NO_POSITION){
-                        listener!!.onItemLongClick(itemView, position)
+                        listener.onItemLongClick(itemView, position)
                     }
                 }
 
                 return@setOnLongClickListener true
+            }
+
+            imbPopUp.setOnClickListener {
+                listener?.let {listener->
+                    val position = adapterPosition
+                    if (position != RecyclerView.NO_POSITION){
+                        listener.onThreeDotClick(itemView, position)
+                    }
+                }
             }
         }
 
@@ -77,15 +86,7 @@ class RecyclerViewTaskAdapter(val context : Context) : RecyclerView.Adapter<Recy
                         setColorFilter(Color.BLUE)
                     }
 
-                    textViewDateTaskPreview.apply {
-                        val formattedString = resources.getString(
-                            R.string.completed_task_string,
-                            completionDate
-                                ?.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM))
-                        )
-                        text = formattedString
-                        setTextColor(Color.BLUE)
-                    }
+                    customTextViewDatePreview(completionDate)
                 }
 
 
@@ -95,7 +96,7 @@ class RecyclerViewTaskAdapter(val context : Context) : RecyclerView.Adapter<Recy
                     }
 
                     textviewDescTaskPreview.apply {
-                        paintFlags = paintFlags and Paint.STRIKE_THRU_TEXT_FLAG
+                        paintFlags = paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
                     }
 
                     imageViewTask.apply {
@@ -106,9 +107,27 @@ class RecyclerViewTaskAdapter(val context : Context) : RecyclerView.Adapter<Recy
                     textViewDateTaskPreview.apply {
                         text = appointedDate
                             .format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))
+                        setTextColor(context.resources.getColor(R.color.grey_default, context.theme))
                     }
                 }
-                else -> {}
+            }
+        }
+
+        private fun customTextViewDatePreview(completionDate: LocalDate?) = with(binding){
+            val dateStr = completionDate?.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))
+            textViewDateTaskPreview.apply {
+
+                text = when(layoutEnum){
+                    LayoutEnum.LINEAR -> { resources.getString(R.string.completed_task_string_full, dateStr) }
+                    LayoutEnum.GRID -> { resources.getString(R.string.completed_task_string_short, dateStr) }
+                }
+
+                textSize = when(layoutEnum){
+                    LayoutEnum.LINEAR ->{ 15f }
+                    LayoutEnum.GRID ->  { 11.5f }
+                }
+
+                setTextColor(Color.BLUE)
             }
         }
     }
@@ -137,13 +156,41 @@ class RecyclerViewTaskAdapter(val context : Context) : RecyclerView.Adapter<Recy
         notifyItemRemoved(position)
     }
 
+    fun updateTask(position: Int, taskInterim: TaskInterim){
+        taskList[position] = taskInterim
+        notifyItemChanged(position)
+    }
+
+    fun setAdapterList(newList : List<TaskInterim>){
+        taskList.clear()
+        taskList.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+
     override fun getItemId(position: Int): Long {
         return taskList[position].id.toLong()
+    }
+
+    fun getItem(position: Int) : TaskInterim{
+        return taskList[position]
+    }
+
+    fun updateTaskFlag(position: Int, flag : Boolean, completionDate: LocalDate){
+        taskList[position].isChecked = flag
+        taskList[position].completionDate = completionDate
+        notifyItemChanged(position)
+    }
+
+    fun updateLayout(layout : LayoutEnum){
+        layoutEnum = layout
+        notifyDataSetChanged()
     }
 
     interface OnItemClickListener{
         fun onItemClick(itemView : View, position : Int)
         fun onItemLongClick(itemView : View, position : Int)
+        fun onThreeDotClick(itemView : View, position: Int)
     }
 
     fun setOnItemClickListener(listener: OnItemClickListener){
