3a50994164f709b2335e0c17c84eec309e4e3d8d
Fazer777 <m.soup2016@yandex.ru>
2024-03-12 21:21:13 +0300

    Update ISubTaskDao.kt

--- data/src/main/java/com/project/data/database/dao/dao_interface/ISubTaskDao.kt
+++ data/src/main/java/com/project/data/database/dao/dao_interface/ISubTaskDao.kt
@@ -3,9 +3,9 @@ package com.project.data.database.dao.dao_interface
 import com.project.data.models.SubTask
 
 interface ISubTaskDao {
-    fun addSubTask(subTasks : List<SubTask>) : List<Int>
+    suspend fun addSubTask(taskId : Int, subTasks : List<SubTask>) : List<Int>
 
-    fun deleteSubTask (taskId : Int)
+    suspend fun deleteSubTask (taskId : Int)
 
-    fun getSubTasks() : List<SubTask>
+    suspend fun getSubTasks() : List<SubTask>
 }
