13ab7d2db89febc4b3bf7c18d89a35cdd3cd6854
Fazer777 <m.soup2016@yandex.ru>
2024-03-12 21:21:05 +0300

    Update ICategoryDao.kt

--- data/src/main/java/com/project/data/database/dao/dao_interface/ICategoryDao.kt
+++ data/src/main/java/com/project/data/database/dao/dao_interface/ICategoryDao.kt
@@ -3,7 +3,7 @@ package com.project.data.database.dao.dao_interface
 import com.project.data.models.Category
 
 interface ICategoryDao {
-    fun addCategory(category: Category) : Unit
-    fun deleteCategory(itemIndex : Int)
-    fun getCategories() : List<Category>
+    suspend fun addCategory(category: Category) : Unit
+    suspend fun deleteCategory(itemIndex : Int)
+    suspend fun getCategories() : List<Category>
 }
