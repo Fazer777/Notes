e5e2bef146b66f6c3fa0a319f25cc24481353647
msoup <m.soup2016@yandex.ru>
2024-04-20 13:54:19 +0300

    - updated methods for use cases

--- app/src/main/java/com/project/taskplanner/presentation/viewmodels/notes/FragmentNotesVM.kt
+++ app/src/main/java/com/project/taskplanner/presentation/viewmodels/notes/FragmentNotesVM.kt
@@ -1,45 +1,31 @@
 package com.project.taskplanner.presentation.viewmodels.notes
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.project.domain.models.NoteInterim
-import com.project.domain.usecase.notes.AddNoteUseCase
-import com.project.domain.usecase.notes.DeleteNoteUseCase
-import com.project.domain.usecase.notes.UpdateNoteUseCase
-import com.project.domain.usecase.notes.GetNotesUseCase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import com.project.domain.models.note.NoteParam
+import com.project.domain.usecase.note.DeleteNoteUseCase
+import com.project.domain.usecase.note.GetNotesUseCase
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class FragmentNotesVM (
-    private val addNoteUseCase: AddNoteUseCase,
-    private val updateNoteUseCase: UpdateNoteUseCase,
     private val deleteNoteUseCase: DeleteNoteUseCase,
     private val getNotesUseCase: GetNotesUseCase
 
 ): ViewModel() {
 
-    private val notesMutableLive = MutableLiveData<ArrayList<NoteInterim>>()
-    val notesLive : LiveData<ArrayList<NoteInterim>> = notesMutableLive
+    val notes : LiveData<List<NoteParam>> = getNotesLive()
 
 
-    suspend fun onAddNoteEvent(noteInterim: NoteInterim) {
-        addNoteUseCase.execute(noteInterim)
+    fun deleteNote(noteParam: NoteParam){
+        viewModelScope.launch{
+            deleteNoteUseCase.execute(noteParam = noteParam)
+        }
     }
 
-    suspend fun onUpdateNoteEvent(noteInterim: NoteInterim) {
-        updateNoteUseCase.execute(noteInterim)
-    }
-
-    suspend fun onDeleteNoteEvent(itemIndex : Int) {
-        deleteNoteUseCase.execute(itemIndex)
-    }
-
-    suspend fun getNotes() {
-        notesMutableLive.postValue(getNotesUseCase.execute())
+    private fun getNotesLive() : LiveData<List<NoteParam>>{
+        return getNotesUseCase.execute().asLiveData()
     }
 
 
