28d0872e25780178bf29c23ca91b1afea73e6746
msoup <m.soup2016@yandex.ru>
2024-04-20 12:50:42 +0300

    the low-level sqlite code and all related code have been removed

--- domain/src/main/java/com/project/domain/repository/ICategoryRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.project.domain.repository
-
-import com.project.domain.models.CategoryInterim
-
-interface ICategoryRepository {
-    suspend fun addCategory(categoryInterim: CategoryInterim)
-    suspend fun deleteCategory(itemIndex : Int)
-    suspend fun getCategories() : List<CategoryInterim>
-}
--- domain/src/main/java/com/project/domain/repository/ISubtaskRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.project.domain.repository
-
-import com.project.domain.models.SubTaskInterim
-
-interface ISubtaskRepository {
-    suspend fun addSubtask(taskId : Int, subTasks: List<SubTaskInterim>) : List<Int>
-    suspend fun deleteSubtask(taskId: Int)
-    suspend fun getSubtasks() : List<SubTaskInterim>
-}
