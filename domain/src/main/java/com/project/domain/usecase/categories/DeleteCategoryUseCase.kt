87ecd4fcca24a9b32f3cc30f077f8641142a844f
msoup <m.soup2016@yandex.ru>
2024-04-20 12:56:03 +0300

    Deleted - old version

--- domain/src/main/java/com/project/domain/usecase/categories/DeleteCategoryUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.project.domain.usecase.categories
-
-import com.project.domain.repository.ICategoryRepository
-
-class DeleteCategoryUseCase(private val categoryRepository: ICategoryRepository) {
-    suspend fun execute(itemIndex : Int){
-        categoryRepository.deleteCategory(itemIndex)
-    }
-}
--- domain/src/main/java/com/project/domain/usecase/tasks/DeleteTaskUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.project.domain.usecase.tasks
-
-import com.project.domain.repository.ITaskRepository
-
-class DeleteTaskUseCase(private val taskRepository: ITaskRepository) {
-    suspend fun execute(taskId : Int){
-        taskRepository.deleteTask(taskId = taskId)
-    }
-}
