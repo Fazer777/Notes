94ae5b6b66f39497b9c0f8a12c28949eb7145299
msoup <m.soup2016@yandex.ru>
2024-04-20 12:54:08 +0300

    - The new version Room library has been added - Gson library has been added

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -38,12 +39,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
 
-    // RoomComponents
-    runtimeOnly("androidx.room:room-ktx:2.5.2")
-    runtimeOnly("androidx.room:room-runtime:2.5.2")
-    implementation("androidx.room:room-testing:2.5.2")
-
     implementation(project(":domain"))
+
+    // Room
+    api("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
