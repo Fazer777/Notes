956839fad96950ece19a2f6e9fe41ee46aef8c7e
msoup <m.soup2016@yandex.ru>
2024-04-20 13:54:05 +0300

    - updated methods for use cases

--- app/src/main/java/com/project/taskplanner/presentation/viewmodels/tasks/FragmentTaskVM.kt
+++ app/src/main/java/com/project/taskplanner/presentation/viewmodels/tasks/FragmentTaskVM.kt
@@ -3,47 +3,42 @@ package com.project.taskplanner.presentation.viewmodels.tasks
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.project.data.models.Task
-import com.project.domain.models.TaskInterim
-import com.project.domain.usecase.tasks.AddTaskUseCase
-import com.project.domain.usecase.tasks.CompleteTaskUseCase
-import com.project.domain.usecase.tasks.DeleteTaskUseCase
-import com.project.domain.usecase.tasks.GetTaskUseCase
-import com.project.domain.usecase.tasks.UpdateTaskUseCase
-import kotlinx.coroutines.Dispatchers
+import com.project.domain.models.task.TaskParam
+import com.project.domain.usecase.task.CompleteTaskUseCase
+import com.project.domain.usecase.task.DeleteTaskUseCase
+import com.project.domain.usecase.task.GetTaskUseCase
+import com.project.taskplanner.presentation.fragments.tasks.LayoutEnum
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 class FragmentTaskVM(
-    private val addTaskUseCase: AddTaskUseCase,
     private val getTaskUseCase: GetTaskUseCase,
-    private val updateTaskUseCase: UpdateTaskUseCase,
     private val deleteTaskUseCase: DeleteTaskUseCase,
     private val completeTaskUseCase: CompleteTaskUseCase
 ) : ViewModel() {
 
-    suspend fun onAddTaskButtonClicked(createdTask : TaskInterim) : Int{
-       return addTaskUseCase.execute(taskInterim =  createdTask)
+    private val layoutStateMutableLive = MutableLiveData<LayoutEnum>()
+    val layoutStateLive : LiveData<LayoutEnum> = layoutStateMutableLive
+
+     fun deleteTask(taskParam: TaskParam){
+        viewModelScope.launch {
+            deleteTaskUseCase.execute(taskParam = taskParam)
+        }
     }
 
-    suspend fun onUpdateTaskButtonClicked(updatedTask: TaskInterim){
-        updateTaskUseCase.execute(newTask = updatedTask)
+    fun completeTask(taskId: Int, isChecked : Boolean, completionDate : LocalDate?){
+        viewModelScope.launch {
+            completeTaskUseCase.execute(taskId = taskId, flag = isChecked, completionDate = completionDate)
+        }
     }
 
-    suspend fun onDeleteTaskButtonClicked(taskId : Int){
-        deleteTaskUseCase.execute(taskId = taskId)
+    fun getTasks() : LiveData<List<TaskParam>> {
+        return getTaskUseCase.execute().asLiveData()
     }
 
-    suspend fun onThreeDotButtonClicked(taskId: Int, flag : Boolean, completionDate : LocalDate){
-        completeTaskUseCase.execute(
-            taskId = taskId,
-            flag = flag,
-            completionDate = completionDate
-        )
-    }
-
-    suspend fun getTasks() : List<TaskInterim> {
-        return getTaskUseCase.execute()
+    fun changeLayoutState(state : LayoutEnum){
+        layoutStateMutableLive.value = state
     }
 }
